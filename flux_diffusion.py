238d2b39a92c304ce78c7cb84f6483e317492972
ibtsamraza <138867929+ibtsamraza@users.noreply.github.com>
2025-08-07 11:22:46 +0000

    Update flux_diffusion.py

--- flux_diffusion.py
+++ flux_diffusion.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
-headers = {"Authorization": "Bearer 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"} 
+headers = {"Authorization": "Bearer hf_"} 
 
 
 def query(payload):
